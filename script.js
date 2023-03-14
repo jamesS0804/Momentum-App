467ed712b7267c8069fa737b85c6cc0dcbaef580
jamesS0804 <sucaldito.james0804@gmail.com>
2023-03-14 19:59:52 +0800

    Removed a lot of redundant spaces Moved script tag in HTML file to the header to make use of defer Removed unnecessary 'return false' in form tag

--- script.js
+++ script.js
@@ -1,25 +1,18 @@
 const overlay_container = document.querySelector('.overlay')
 const overlay_name = document.querySelector('#overlay-name')
 const overlay_submit = document.querySelector('#overlay-submit')
-
 const main = document.getElementsByTagName('main')[0]
 let user_name = document.querySelector('#user-name')
-
 const clock_container = document.querySelector('.clock')
 const greeting_container = document.querySelector('.greeting-container')
 const circle_container = document.querySelectorAll('.circle-container')
-
 const clock_circle = circle_container[0]
 const greeting_circle = circle_container[1]
-
 const clock_dropdown = document.querySelector('.clock-dropdown')
 const clock_format = document.querySelector('#clock-format')
-
 const name_change = document.querySelector('.name-change')
-
 const todo_name = document.querySelector('#todo-name')
 const todo_list = document.querySelector('.todolist-container')
-
 document.addEventListener('DOMContentLoaded', ()=>{
     if(localStorage.getItem('hasData')){
         main.classList.remove('unshow')
@@ -29,7 +22,6 @@ document.addEventListener('DOMContentLoaded', ()=>{
         console.log("no data!")
     }
 })
-
 function submit(){
     let name_input = overlay_name.value
 
@@ -41,19 +33,16 @@ function submit(){
     main.classList.remove('unshow')
     overlay_container.classList.add('unshow')
 }
-
 overlay_submit.addEventListener('click', (e)=> {
     e.preventDefault()
     submit()
 })
-
 overlay_name.addEventListener('keypress', (e)=>{
     if(e.key === 'Enter'){
         e.preventDefault()
         submit()
     }
 })
-
 function getCurrentTime() {
     const hour = document.querySelector('#hours')
     const minutes = document.querySelector('#minutes')
@@ -89,7 +78,6 @@ function getCurrentTime() {
      
     setTimeout(()=>{getCurrentTime()},1000)
 }
-
 function getRandomQuote() {
     const quote_text = document.querySelector('#text')
     const quote_author = document.querySelector('#author')
@@ -105,9 +93,7 @@ function getRandomQuote() {
         });
     setTimeout(()=>{getRandomQuote()}, 60000)
 }
-
 let i=0;
-
 function changeBackgroundImage(index) {  
     // let authorization = '8I1-nFG0oPbsDbN3MCGILQuV1gySyv2Y7PtbJFKO50E'
     // let url = `https://api.unsplash.com/photos/?client_id=${authorization}&query=lion`
@@ -121,47 +107,33 @@ function changeBackgroundImage(index) {
     //         let image_url = data[random].urls.raw
     //         document.body.style.backgroundImage = `url('${image_url}')`
     //     });
-
     const images = ['./images/sunrise.jpg','./images/bridge.jpg','./images/city.jpg','./images/dunes.png','./images/retro.jpg','./images/space.png']
-
     document.body.style.backgroundImage = `url('${images[index]}')`
     index++
     index = index < images.length ? index : 0
     setTimeout(()=>{changeBackgroundImage(index)}, 60000)
 }
-
 clock_container.addEventListener('mousemove', ()=>{
     clock_circle.classList.remove('unshow')
 })
-
 clock_container.addEventListener('mouseout', ()=>{
     clock_circle.classList.add('unshow')
 })
-
 greeting_container.addEventListener('mousemove', ()=>{
     greeting_circle.classList.remove('unshow')
 })
-
 greeting_container.addEventListener('mouseout', ()=>{
     greeting_circle.classList.add('unshow')
 })
-
 clock_circle.addEventListener('click', ()=>{
     clock_dropdown.classList.toggle('visible')
 })
-
 greeting_circle.addEventListener('click', ()=>{
     name_change.classList.toggle('visible')
 })
-
 todo_name.addEventListener('click', ()=>{
     todo_list.classList.toggle('unshow')
 })
-
 getCurrentTime()
 getRandomQuote()
 changeBackgroundImage(i)
-
-
-
-
