3c48c3982499d2320244673854397186109c970d
jamesS0804 <sucaldito.james0804@gmail.com>
2023-03-20 01:30:20 +0800

    Fixed CSS on buttons Added name change feature Added adding of item on todo list
    Pending todolist localstorage feature

--- script.js
+++ script.js
@@ -3,25 +3,33 @@ const overlay_name = document.querySelector('#overlay-name')
 const overlay_submit = document.querySelector('#overlay-submit')
 const main = document.getElementsByTagName('main')[0]
 let user_name = document.querySelector('#user-name')
+
 const clock_container = document.querySelector('.clock')
-const greeting_container = document.querySelector('.greeting-container')
+const greeting_container = document.querySelector('.greeting')
 const circle_container = document.querySelectorAll('.circle-container')
+
 const clock_circle = circle_container[0]
 const greeting_circle = circle_container[1]
+
 const clock_dropdown = document.querySelector('.clock-dropdown')
 const clock_format = document.querySelector('#clock-format')
 const name_change = document.querySelector('.name-change')
 const todo_name = document.querySelector('#todo-name')
 const todo_list = document.querySelector('.todolist-container')
+
+const list = document.querySelector('#list')
+const input_item = document.querySelector('#input-item')
+
+let i=0;
+
 document.addEventListener('DOMContentLoaded', ()=>{
     if(localStorage.getItem('hasData')){
         main.classList.remove('unshow')
         overlay_container.classList.add('unshow')
         user_name.innerText = localStorage.getItem('name')
-    } else {
-        console.log("no data!")
     }
 })
+
 function submit(){
     let name_input = overlay_name.value
 
@@ -33,16 +41,19 @@ function submit(){
     main.classList.remove('unshow')
     overlay_container.classList.add('unshow')
 }
+
 overlay_submit.addEventListener('click', (e)=> {
     e.preventDefault()
     submit()
 })
+
 overlay_name.addEventListener('keypress', (e)=>{
     if(e.key === 'Enter'){
         e.preventDefault()
         submit()
     }
 })
+
 function getCurrentTime() {
     const hour = document.querySelector('#hours')
     const minutes = document.querySelector('#minutes')
@@ -54,18 +65,15 @@ function getCurrentTime() {
     let curr_hour = date.getHours()
     let curr_minutes = date.getMinutes()
     let curr_seconds = date.getSeconds()
-    
-    // console.log(date = date.toLocaleString('en-US', {hour12:false}))
-
 
     session.innerHTML = curr_hour > 11 ? "PM" : "AM"
 
     time_of_day.innerHTML = curr_hour < 12 ? "morning"
                                         : curr_hour < 18 ? "afternoon"
                                         : "evening"
-
     if(!clock_format.checked){
         curr_hour = curr_hour % 12
+        curr_hour = curr_hour % 12 === 0 ? 12 : curr_hour
         session.style.display = 'block'
     } else { 
         session.style.display = 'none'
@@ -74,10 +82,9 @@ function getCurrentTime() {
     hour.innerHTML = curr_hour < 10 ? '0' + curr_hour : curr_hour 
     minutes.innerHTML = curr_minutes < 10 ? '0' + curr_minutes : curr_minutes 
     seconds.innerHTML = curr_seconds < 10 ? '0' + curr_seconds : curr_seconds 
-
-     
     setTimeout(()=>{getCurrentTime()},1000)
 }
+
 function getRandomQuote() {
     const quote_text = document.querySelector('#text')
     const quote_author = document.querySelector('#author')
@@ -93,47 +100,99 @@ function getRandomQuote() {
         });
     setTimeout(()=>{getRandomQuote()}, 60000)
 }
-let i=0;
-function changeBackgroundImage(index) {  
-    // let authorization = '8I1-nFG0oPbsDbN3MCGILQuV1gySyv2Y7PtbJFKO50E'
-    // let url = `https://api.unsplash.com/photos/?client_id=${authorization}&query=lion`
-    
-    // fetch(url)
-    //     .then(response => {
-    //         return response.json();
-    //     })
-    //     .then(data => {
-    //         let random = Math.floor(Math.random() * (data.length + 1))
-    //         let image_url = data[random].urls.raw
-    //         document.body.style.backgroundImage = `url('${image_url}')`
-    //     });
-    const images = ['./images/sunrise.jpg','./images/bridge.jpg','./images/city.jpg','./images/dunes.png','./images/retro.jpg','./images/space.png']
-    document.body.style.backgroundImage = `url('${images[index]}')`
-    index++
-    index = index < images.length ? index : 0
+
+function changeBackgroundImage(index) {
+    fetch('./images.json')
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            document.body.style.backgroundImage = `url('${data.images[index]}')`
+            index++
+            index = index < data.images.length ? index : 0
+        });
     setTimeout(()=>{changeBackgroundImage(index)}, 60000)
+}
+function onClickOutside() {
+    document.addEventListener('click', e => {
+       if(!e.target.id) {
+            clock_dropdown.classList.add('unshow')
+            name_change.classList.add('unshow')
+            clock_circle.classList.add('unshow')
+            greeting_circle.classList.add('unshow')
+            setUserName()
+       }
+    });
+}
+function setUserName() {
+    let new_name = user_name.innerText
+    console.log(`new_name: ${new_name}`)
+    localStorage.setItem('name', new_name)
+    user_name.blur()
 }
 clock_container.addEventListener('mousemove', ()=>{
     clock_circle.classList.remove('unshow')
 })
 clock_container.addEventListener('mouseout', ()=>{
-    clock_circle.classList.add('unshow')
+    !clock_dropdown.classList.contains('unshow') ? clock_circle.classList.remove('unshow') : clock_circle.classList.add('unshow')
 })
 greeting_container.addEventListener('mousemove', ()=>{
     greeting_circle.classList.remove('unshow')
 })
 greeting_container.addEventListener('mouseout', ()=>{
-    greeting_circle.classList.add('unshow')
+    !name_change.classList.contains('unshow') ?  greeting_circle.classList.remove('unshow') : greeting_circle.classList.add('unshow')
 })
 clock_circle.addEventListener('click', ()=>{
-    clock_dropdown.classList.toggle('visible')
+    !clock_dropdown.classList.contains('unshow') ? clock_dropdown.classList.add('unshow') : clock_dropdown.classList.remove('unshow')
 })
 greeting_circle.addEventListener('click', ()=>{
-    name_change.classList.toggle('visible')
+    !name_change.classList.contains('unshow') ? name_change.classList.add('unshow') : name_change.classList.remove('unshow')
 })
 todo_name.addEventListener('click', ()=>{
     todo_list.classList.toggle('unshow')
 })
+user_name.addEventListener('keypress', (e)=>{
+    if(e.key === 'Enter'){
+        e.preventDefault()
+        setUserName()
+    }
+})
+name_change.addEventListener('click', ()=>{
+    name_change.classList.add('unshow')
+    user_name.focus()
+})
+
+let itemArray = []
+
+function Item (isChecked,item) {
+    this.isChecked = isChecked
+    this.item = item
+}
+
+function addItem(input) {
+    console.log(list.innerHTML)
+    let addedItem = `
+        <div>
+            <input type="checkbox">
+            <span>${input}</span>
+        </div>
+    `
+    let recordItem = new Item(false,input)
+    itemArray.push(recordItem)
+    console.log(itemArray)
+    localStorage.setItem('list', itemArray)
+    list.insertAdjacentHTML('beforeend',addedItem)
+}
+input_item.addEventListener('keypress', (e)=>{
+    if(e.key === 'Enter'){
+        e.preventDefault()
+        let input = input_item.value
+        console.log(`input: ${input}`)
+        addItem(input)
+        input_item.value = ""
+    }
+})
 getCurrentTime()
 getRandomQuote()
 changeBackgroundImage(i)
+onClickOutside()
